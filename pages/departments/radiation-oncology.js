e20f6abdc4c0a89ea46ba401f602afc758e19a75
naveddevleper <navedahamad2016@gmail.com>
2023-02-11 13:35:13 +0530

    images removed

--- pages/departments/radiation-oncology.js
+++ pages/departments/radiation-oncology.js
@@ -26,7 +26,7 @@ const RadiationOncology = () => {
                     <div className="row">
                         <div className="col-lg-5">
                             <div className="doctor-details-item doctor-details-left">
-                                <img src="/images/departments/image-16.png" alt="Doctor" />
+                                <img src="/images/departments/radiation-onco.webp" alt="Doctor" />
 
                                 <div className="doctor-details-contact">
                                     <h3>Contact info</h3>
